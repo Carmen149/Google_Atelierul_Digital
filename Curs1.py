3742bb476dd55c2e5605d7fcd9eb5476ca2464ac
Carmen149 <bontea_carmen@yahoo.com>
2022-04-05 18:58:18 +0300

    new code

--- Curs1.py
+++ Curs1.py
@@ -1,2 +1,10 @@
 print("Cursul 1 Python")
 print("new Branch created")
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+my_sliced_list = my_list[2:-5]
+print(my_sliced_list)
+
+my_dict = {"key_1" : 1, "key_2" : 2, "key_3" : 3}
+print(my_dict.keys())
+print(my_dict.values());
+print(my_dict.items());
